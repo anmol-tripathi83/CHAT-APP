8055379e487daae300249b7dc836bdf521049a43
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2025-08-23 11:57:02 +0530

    Auth route for login and logout done

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { generateToken } from "../lib/utills.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
+
 export const signup = async (req, res)=>{
     const {fullName, email, password} = req.body;
     try{
@@ -50,12 +51,45 @@ export const signup = async (req, res)=>{
 
 
 export const login = async (req, res)=>{
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+        // if user is already not present in the DB
+        if(!user){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
 
+        // if email found in the DB then compare its password with stored password for authenticity
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+           return res.status(400).json({message: "Invalid credentials"}); 
+        }
+
+        // if email and password staisfy their cond
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic : user.profilePic,
+        });
+    } catch(error){
+        console.log("Error in login controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
 }
 
 
 export const logout = (req, res)=>{
-
+    // to logout just clear the cookie it by default get log out
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message: "Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
 }
 
 
@@ -65,5 +99,5 @@ export const updateProfile = async (req, res) => {
 
 
 export const checkAuth = (req, res) => {
-    
+
 }
