78a88820ba7faa129a4cbd13be1597b594ff37d4
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2025-08-23 13:14:44 +0530

    Added routes for diff pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,25 @@
 // rafc command to get boiler plate for arrow function
 
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import { HomePage }  from "./Pages/HomePage";      // export default => HomePage and export only => {HomePage}
+import { SignupPage } from "./Pages/SignupPage";
+import { SettingPage } from "./Pages/SettingPage";
+import { ProfilePage } from "./Pages/ProfilePage";
+import { LoginPage } from "./Pages/LoginPage";
+
 const App = () => {
     return (
-        <div>Chat-App</div>
+        <div >
+       <Navbar/>
+        <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/signup" element={<SignupPage/>} />
+            <Route path="/login" element={<LoginPage/>} />
+            <Route path="/setting" element={<SettingPage/>} />
+            <Route path="/profile" element={<ProfilePage/>} />
+        </Routes>
+    </div>
     );
 }
 
