5c5ebb3efa853cec1df64f1241fda4f01367e413
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2025-08-23 22:22:10 +0530

    Revert "Completed Signup, login, logout pages"
    This reverts commit 3d73be4cbcc1d8c49ba6f5a1702225da66be23cb.

--- Frontend/src/lib/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",    // backend url
-    withCredentials: true,    // send the cookies in every request
-});
