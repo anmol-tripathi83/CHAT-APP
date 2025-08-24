03c3932087b80d7a6e4a548a33bcf875370dcab6
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2025-08-25 02:32:12 +0530

    Added some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
        <Navbar/>
        <Routes>
         <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>} />
-        <Route path="/signup" element={!authUser ? <SignupPage/> : <Navigate to="/login"/>} />
+        <Route path="/signup" element={!authUser ? <SignupPage/> : <Navigate to="/"/>} />
         <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/" />} />
         <Route path="/setting" element={<SettingPage/>} />
         <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>} />
